fcf425b43658955095146c2f2c47a2250ac35a75
JMFStorm <jannematiasf@gmail.com>
2023-11-10 17:43:38 +0200

    builds

--- build.bat
+++ build.bat
@@ -13,10 +13,14 @@ set LIB_DIR=lib
 set OUTPUT_DIR=debug_build
 set OUTPUT_FILE=Engine3D.exe
 
-set SOURCE_FILES=%SOURCE_DIR%\*.cpp
-set HEADER_FILES=%INCLUDE_DIR%\*.h %INCLUDE_DIR%\*.hpp
-set LIBRARY_FILES=%LIB_DIR%\*.lib
+set SOURCE_FILES=src\main.cpp %SOURCE_DIR%\*.cpp
+set IMGUI_FILES=imgui\*.cpp
 
-cl /EHsc /std:c++20 src\main.cpp /I%INCLUDE_DIR% /link /SUBSYSTEM:CONSOLE /NODEFAULTLIB:MSVCRT /LIBPATH:%LIB_DIR% glfw3.lib libglew32.lib glew32.lib opengl32.lib freetype.lib
+set NODEFAULTS=/NODEFAULTLIB:MSVCRT /NODEFAULTLIB:LIBCMT
+set LINKED_LIBRARIES=glfw3.lib libglew32.lib glew32.lib opengl32.lib freetype.lib user32.lib kernel32.lib shell32.lib gdi32.lib
+
+REM glfw3.lib;opengl32.lib;kernel32.lib;user32.lib;;winspool.lib;shell32.lib;ole32.lib;oleaut32.lib;uuid.lib;comdlg32.lib;advapi32.lib;glfw3.lib
+
+cl /Fo.\build\obj\ /Febuild\Engine3D.exe /std:c++20 /EHsc /MDd %SOURCE_FILES% %IMGUI_FILES% /I%INCLUDE_DIR% /link %NODEFAULTS% /LIBPATH:%LIB_DIR% %LINKED_LIBRARIES%
 
 endlocal
