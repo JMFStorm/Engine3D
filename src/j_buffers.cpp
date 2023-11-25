c6eeb84454bf25dfe722cf46a60ab074c1c19dc7
JMFStorm <jannematiasf@gmail.com>
2023-11-25 09:30:16 +0200

    git commit

--- src/j_buffers.cpp
+++ src/j_buffers.cpp
@@ -8,8 +8,28 @@ void memory_buffer_mallocate(MemoryBuffer* buffer, s64 size_in_bytes, char* name
 	buffer->memory = (byte*)malloc(size_in_bytes);
 	memset(buffer->memory, 0x00, size_in_bytes);
 	buffer->size = size_in_bytes;
+	buffer->used_sub_allocation_capacity = 0;
 	strcpy_s(buffer->name, name);
 	printf("memory_buffer_mallocate(): %.3f KB allocated for buffer: %s.\n", (float)size_in_bytes / (float)1024, name);
+}
+
+MemoryBuffer memory_buffer_suballocate(MemoryBuffer* buffer, s64 size_in_bytes)
+{
+	ASSERT_TRUE(
+		buffer->size + size_in_bytes <= buffer->size - buffer->used_sub_allocation_capacity,
+		"Arena has size of suballocation");
+
+	s64 memory_index = sizeof(byte) * buffer->used_sub_allocation_capacity;
+	byte* sub_alloc_mem_start = &buffer->memory[memory_index];
+	buffer->used_sub_allocation_capacity += size_in_bytes;
+
+	MemoryBuffer sub_allocation = {
+		.name = "",
+		.size = size_in_bytes,
+		.used_sub_allocation_capacity = 0,
+		.memory = sub_alloc_mem_start
+	};
+	return sub_allocation;
 }
 
 void memory_buffer_wipe(MemoryBuffer* buffer)
--- src/j_buffers.h
+++ src/j_buffers.h
@@ -5,9 +5,11 @@
 typedef struct MemoryBuffer {
 	char name[32];
 	s64 size;
+	s64 used_sub_allocation_capacity;
 	byte* memory;
 } MemoryBuffer;
 
 void memory_buffer_mallocate(MemoryBuffer* buffer, s64 size_in_bytes, char* name);
+MemoryBuffer memory_buffer_suballocate(MemoryBuffer* buffer, s64 size_in_bytes);
 void memory_buffer_wipe(MemoryBuffer* buffer);
 void memory_buffer_free(MemoryBuffer* buffer);
