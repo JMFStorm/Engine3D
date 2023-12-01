1fe11bd988c2b116ac3b39a5797ea2299b112b9a
JMFStorm <jannematiasf@gmail.com>
2023-12-01 21:14:24 +0200

    git commit

---
+++ build.bat
@@ -0,0 +1,25 @@
+@echo off
+
+if not defined VC_ENV_SET (
+	call "G:\Visual Studio 2022\VC\Auxiliary\Build\vcvars64.bat"
+	set VC_ENV_SET=1
+)
+
+setlocal
+
+set SOURCE_DIR=src
+set INCLUDE_DIR=include
+set LIB_DIR=lib
+
+set SOURCE_FILES=%SOURCE_DIR%\*.cpp
+set IMGUI_FILES=imgui\*.cpp
+
+set NODEFAULTS=/NODEFAULTLIB:MSVCRT
+set BUILD_FLAGS=/DEBUG /INCREMENTAL
+set LINKED_LIBRARIES=glfw3.lib libglew32.lib glew32.lib opengl32.lib freetype.lib kernel32.lib gdi32.lib shell32.lib Comdlg32.lib
+
+REM NEEDED(?) => winspool.lib ole32.lib oleaut32.lib uuid.lib advapi32.lib user32.lib
+
+cl /Fo.\debug\obj\ /std:c++20 /EHsc /Zi /Od /MTd /MP %SOURCE_FILES% %IMGUI_FILES% /I%INCLUDE_DIR% /link %BUILD_FLAGS% /OUT:debug/Engine3D_debug.exe %NODEFAULTS% /LIBPATH:%LIB_DIR% %LINKED_LIBRARIES%
+
+endlocal
